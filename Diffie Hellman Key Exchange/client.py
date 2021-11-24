c88e75fa5b42728339a47d07b7e01811acd9fb85
Akshayjain-2812 <49474573+Akshayjain-2812@users.noreply.github.com>
2021-11-24 20:19:35 +0530

    Add files via upload

---
+++ Diffie Hellman Key Exchange/client.py
@@ -0,0 +1,21 @@
+# Diffie Hellman client side
+import random
+from server import *
+
+# D.H. parameters
+g = 2           #primitive root
+p = 65521       #commonly agreed prime integer
+
+# Alice - client
+# Alice picks a random exponent a between 1 and p:
+# (this is Alice's private number)
+a = random.randint(1, p)
+# Alice computes her public number A = g^a mod p:
+A = g**a % p
+
+##################Exchange Bob public key#################
+
+# Alice computes the shared secret, which is B^a (from her point of view)
+shared_secret_Alices_POV = B**a % p
+#print(shared_secret_Alices_POV)
+#print("client \n")
